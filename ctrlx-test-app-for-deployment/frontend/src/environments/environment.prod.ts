e05ac1a19aec4170362978f8cc44b17c3ea7a75c
Silas Kuschke <Silas.Kuschke@boschrexroth.de>
2026-06-16 15:12:45 +0200

    Implemented environmental files for the frontend so you can use production or development environment

---
+++ ctrlx-test-app-for-deployment/frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: '/api' // Relative URL für die App auf der ctrlX CORE
+};
---
+++ ctrlx-test-app-for-deployment/frontend/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:5001/api' // URL zum lokalen Flask-Backend
+};
